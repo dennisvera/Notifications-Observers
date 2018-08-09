7258b0dda088b35c81e45314fa7092a1f822ad85
dennisvera <dennisvera.z@gmail.com>
2018-08-09 13:19:48 -0400

    first commit

---
+++ Delegates-Protocols/BaseScreen.swift
@@ -0,0 +1,35 @@
+//
+//  BaseScreen.swift
+//  Delegates-Protocols
+//
+//  Created by Sean Allen on 5/20/17.
+//  Copyright © 2017 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class BaseScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
